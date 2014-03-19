8639fd544510e53a94f934879c9cbbfd76d604ea
Brian David Arpie <brian@crisply.com>
2014-03-19 14:49:58 -0400

    0.0.0.1

--- simple-stocks/main.js
+++ simple-stocks/main.js
@@ -8,73 +8,71 @@
  */
 
 var str,
-	html,
-	url,
-	title,
-	price,
-	headDOM,
-	dataDOM,
-	head,
-	data,
-	query;
+ html,
+ url,
+ title,
+ price,
+ headDOM,
+ dataDOM,
+ head,
+ data,
+ query;
 
 function TableController($scope) {
 
-	$scope.trElements = [{}];
+ $scope.trElements = [{}];
 
-	$scope.addElement = function() {
+ $scope.addElement = function() {
+  $scope.trElements = [{}];
 
-		$scope.trElements = [{}];
+  var symbol = $scope.stockInput,
+   ticker = document.getElementById('ticker_symbol'),
+   company = document.getElementById('company_info');
+	 
+  url = 'http://www.parsedcache.com/simple-proxy/' +
+   'ba-simple-proxy.php?url=http%3A%2F%2F' +
+   'finance.yahoo.com%2Fq%2Fks%3Fs%3D' +
+   symbol +
+   '&full_headers=1&full_status=1';
+  
+  //asynchronous
+  $.ajax({
+   url: url,
+   dataType: 'json',
+   async: false,
+	 success: function(data) {
+    str = JSON.stringify(data, null, 2);
+    html = $.parseHTML(str);
+    query = html[12];
+    headDOM = query.getElementsByClassName('\\\"yfnc_tablehead1\\\"');
+    dataDOM = query.getElementsByClassName('\\\"yfnc_tabledata1\\\"');
+    title = $(query).find('div > div > div > div > h2');
+    price = $(query).find('#\\\\\\\"yfs_l84_' + symbol + '\\\\\\\"');
+    ticker.value = "$" + price.html();
+    company.value = title.html();
+   }
+  });
+	
+  for (var i = 0; i < headDOM.length; i++) {
+   head = headDOM[i].innerHTML;
+   data = dataDOM[i].innerHTML;
 
-		var symbol = $scope.stockInput,
-			ticker = document.getElementById('ticker_symbol'),
-			company = document.getElementById('company_info');
+   if (head.indexOf("<") != -1) {
+    head = head.substring(0, head.indexOf("<"));
+   }
+   head = head.replace("amp;", "");
+   if (data.indexOf("<") != -1) {
+    data = data.substring(data.indexOf(">") + 1, data.lastIndexOf("<"));
+   }
 
+  $scope.trElements.push({
+   text: head,
+   value: data
+  });
 
-		url = 'http://www.parsedcache.com/simple-proxy/' +
-			'ba-simple-proxy.php?url=http%3A%2F%2F' +
-			'finance.yahoo.com%2Fq%2Fks%3Fs%3D' +
-			symbol +
-			'&full_headers=1&full_status=1';
+  $scope.stockInput = '';
 
-		// synchronous ajax call
-		$.ajax({
-			url: url,
-			dataType: 'json',
-			async: false,
-			success: function(data) {
-				str = JSON.stringify(data, null, 2);
-				html = $.parseHTML(str);
-				query = html[12];
-				headDOM = query.getElementsByClassName('\\\"yfnc_tablehead1\\\"');
-				dataDOM = query.getElementsByClassName('\\\"yfnc_tabledata1\\\"');
-				title = $(query).find('div > div > div > div > h2');
-				price = $(query).find('#\\\\\\\"yfs_l84_' + symbol + '\\\\\\\"');
-				ticker.value = "$" + price.html();
-				company.value = title.html();
-
-			}
-		});
-
-		for (var i = 0; i < headDOM.length; i++) {
-			head = headDOM[i].innerHTML;
-			data = dataDOM[i].innerHTML;
-
-			if (head.indexOf("<") != -1) {
-				head = head.substring(0, head.indexOf("<"));
-			}
-			head = head.replace("amp;", "");
-			if (data.indexOf("<") != -1) {
-				data = data.substring(data.indexOf(">") + 1, data.lastIndexOf("<"));
-			}
-
-			$scope.trElements.push({
-				text: head,
-				value: data
-			});
-			
-			$scope.stockInput = '';
-
-		}
-	}
+  }
+ }
+ 
 }
